0af6b0118a578f80e74b6b446a7508a3f46fce81
Leming1488 <borovweb@gmail.com>
2021-01-11 10:11:55 +0700

    add new

--- cypress/integration/authentication_user.spec.js
+++ cypress/integration/authentication_user.spec.js
@@ -1,5 +1,4 @@
 const user = require('../fixtures/user.json');
-const errors = require('../fixtures/errors.json');
 
 describe('Authentication User', () => {
   before(() => {
@@ -13,12 +12,13 @@ describe('Authentication User', () => {
 
   });
 
-  // describe('401 - Если не авторизован', () => {
-  //   it('Переходим на главную страницу, попадаем на страницу логина', () => {
-  //     cy.url().should('include', '/dashboard');
-  //     cy.get('[data-cy=login]').should('exist');
-  //   });
-  // });
+  describe('Если не авторизован', () => {
+    it('Переходим на главную страницу, попадаем на страницу логина', () => {
+      cy.visit('/dashboard');
+      cy.url().should('include', 'login');
+      cy.get('[data-cy=login]').should('exist');
+    });
+  });
 
   describe('Авторизация', () => {
     before(() => {
@@ -27,11 +27,10 @@ describe('Authentication User', () => {
       cy.get('[data-cy=login]').should('exist');
     });
 
-    it('Видим поля формы и они пустые', () => {
+    it('Видим поля формы и они пустые, не видим ошибки', () => {
       cy.get('[data-cy=password]').should('have.value', '');
       cy.get('[data-cy=email]').should('have.value', '');
-      cy.get('[data-cy="email-error"]').should('not.exist');
-      cy.get('[data-cy="password-error"]').should('not.exist');
+      cy.get('[data-cy="email-error"]').should('not.visible');
     });
 
 
@@ -41,6 +40,7 @@ describe('Authentication User', () => {
         .should('have.value', user.invalid_email);
 
       cy.get('[data-cy=auth_form_submit]').click();
+      cy.get('[data-cy="email-error"]').should('be.visible');
     });
 
     it('Заполняем поле password, кликаем по кнопке войти, видим тект ошибки', () => {
@@ -54,6 +54,7 @@ describe('Authentication User', () => {
         .should('have.value', user.invalid_password);
 
       cy.get('[data-cy=auth_form_submit]').click();
+      cy.get('[data-cy="password-error"]').should('be.visible');
     });
 
     it('Заполняем поле email валидным значением, кликаем по кнопке войти, не видим ошибку', () => {
@@ -63,7 +64,7 @@ describe('Authentication User', () => {
         .should('have.value', user.email);
 
       cy.get('[data-cy=auth_form_submit]').click();
-      cy.get('[data-cy="email-error"]').should('not.exist');
+      cy.get('[data-cy="email-error"]').should('not.visible');
     });
 
     it('Заполняем поле password валидным значением (> 6 символов), кликаем по кнопке войти, не видим ошибку', () => {
@@ -73,7 +74,7 @@ describe('Authentication User', () => {
         .should('have.value', user.password);
 
       cy.get('[data-cy=auth_form_submit]').click();
-      cy.get('[data-cy="password-error"]').should('not.exist');
+      cy.get('[data-cy="password-error"]').should('not.visible');
     });
   });
 });
