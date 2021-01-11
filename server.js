9bbdd95e00e8839631a7e8e1279e455fa6ab8427
Leming1488 <borovweb@gmail.com>
2021-01-11 10:23:29 +0700

    fix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const minimist = require('minimist')
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
@@ -10,15 +11,12 @@ const port = minimist(process.argv.slice(2)).port
 app.use(express.static("public"));
 
 
-app.set('views', __dirname)
-app.set('view engine', 'hbs')
-
 app.get('/', (req, res) => res.redirect('/login'))
 
 
 // this is the standard HTML login page
 app.get('/login', (req, res) => {
-  res.render('./login.hbs')
+  res.sendFile((path.join(__dirname + '/login.html')))
 })
 
 app.listen(port)
